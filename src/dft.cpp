592ed9d83dc36eea57c093f0a7e4adc194c75716
dkumar <dkumar@lbl.gov>
2020-02-17 08:11:18 -0800

    cleaning up a little

--- src/dft.cpp
+++ src/dft.cpp
@@ -2,53 +2,44 @@
 #include <pybind11/numpy.h>
 
 #include "complex.h"
+#include "mdscatter.h"
 
 namespace py = pybind11;
 
-#ifdef HAVE_CUDA
-extern void cudft(unsigned, float *,  unsigned, float *, complex_t *);
-#endif // HAVE_CUDA
+py::array cpu_dft(np_array_t Pts, np_array_t qVals) {
 
-py::array py_dft(py::array_t<float, py::array::c_style | py::array::forcecast> Pts,
-				 py::array_t<float, py::array::c_style | py::array::forcecast> qVals) {
+    if ((Pts.ndim() != 2) or qVals.ndim() != 2)
+        throw std::runtime_error("Input arrays must be 2-D numpy arrays");
 
-		if ((Pts.ndim() != 2) or qVals.ndim() != 2)
-			throw std::runtime_error("Input arrays must be 2-D numpy arrays");
+    if ((Pts.shape()[1] != 3) or (qVals.shape()[1] != 3))
+        throw std::runtime_error("input arrays must of shape [N, 3]");
 
-		if ((Pts.shape()[1] != 3) or (qVals.shape()[1] != 3))
-			throw std::runtime_error("input arrays must of shape [N, 3]");
+    /* size of input arrays */
+    unsigned npts = Pts.shape()[0];
+    unsigned nq = qVals.shape()[0];
 
-		/* size of input arrays */
-		unsigned npts = Pts.shape()[0];
-		unsigned nq = qVals.shape()[0];
+    /* NumPy  will allocate the buffer */
+    auto result = py::array_t<complex_t>(nq);
 
-		/* NumPy  will allocate the buffer */
-		auto result = py::array_t<complex_t>(nq);
-
-		py::buffer_info buf1 = Pts.request();
-		py::buffer_info buf2 = qVals.request();
-		py::buffer_info buf3 = result.request();
+    py::buffer_info buf1 = Pts.request();
+    py::buffer_info buf2 = qVals.request();
+    py::buffer_info buf3 = result.request();
 		
-		float * pts = (float *) buf1.ptr;
-		float * qvs = (float *) buf2.ptr;
-		complex_t * ft = (complex_t *) buf3.ptr;
+    float * pts = (float *) buf1.ptr;
+    float * qvs = (float *) buf2.ptr;
+    complex_t * ft = (complex_t *) buf3.ptr;
 
-#ifdef HAVE_CUDA
-		cudft(npts, pts, nq, qvs, ft);		
-#else // HAVE_CUDA
-#pragma omp parallel for
-		for (int i = 0; i < nq; i++) {
-			ft[i] = COMPLX_ZERO;
-			for (int j = 0; j < npts; j++) {
-				float q_r = 0.f;
-				for (int k = 0; k < 3; k++) q_r += qvs[3*i + k] * pts[3*j + k];
-				ft[i] += std::exp(-COMPLX_J * q_r);
-			}
-		}
-#endif
-		return result;
+    #pragma omp parallel for
+    for (int i = 0; i < nq; i++) {
+        ft[i] = COMPLX_ZERO;
+        for (int j = 0; j < npts; j++) {
+            float d = 0;
+            float q_dot_r = 0.f;
+            for (int k = 0; k < 3; k++)
+                q_dot_r += qvs[3*i + k] * pts[3*j + k];
+            ft[i] += std::exp(-COMPLX_J * q_dot_r);
+        }
+    }
+    return result;
 }
 
-PYBIND11_MODULE (mdscatter, m) {
-		m.def("dft", &py_dft, "Compute Discrete Fourier Transform at precribed q-points");
-}
--- src/gpu_dft.cu
+++ src/gpu_dft.cu
@@ -1,28 +1,67 @@
 #include <cuda_runtime.h>
 #include "complex.h"
+#include "mdscatter.h"
 
-const size_t N_PTS = 6144; // 2048 x 3 
+__constant__ float rsq;
+
+
+//const size_t N_PTS = 6144; // 2048 x 3 
+const int N_PTS = 512;
 
 void __global__ gpuDFT(unsigned npts, float * pts, unsigned nq, float * qvals, cucomplex_t * ft) {
-	 
-	const cucomplex_t NEG_I = make_cuFloatComplex(0.f, -1.f);
-	// compute dft
 
-	unsigned i = blockDim.x * blockIdx.x  + threadIdx.x;
+    const cucomplex_t NEG_I = make_cuFloatComplex(0.f, -1.f);
 
-	if (i < nq) {
-		ft[i] = make_cuFloatComplex(0.f, 0.f);
-		for (unsigned j = 0; j < npts; j++) {
-			float q_r = 0;
-			for (unsigned k = 0; k < 3; k++) 
-				q_r += qvals[3 * i + k] * pts[3 * j + k];
-			ft[i] = ft[i] + Cexpf(NEG_I * q_r);
-		}
-	}
+	int idx = blockDim.x * blockIdx.x  + threadIdx.x;
+
+    __shared__ float shamem [N_PTS * 3];
+	int nfills =  npts / N_PTS;
+    if (npts % N_PTS) nfills++;
+
+    int np = N_PTS;
+    for (int ifill = 0; ifill < nfills; ifill++) {
+        int pnt_idx = ifill * N_PTS + threadIdx.x;
+        if ((ifill + 1) * N_PTS > npts) np = npts % N_PTS; 
+
+        if (pnt_idx < npts ) {
+            for (int k = 0; k < 3; k++) 
+                shamem[threadIdx.x * 3 + k] = pts[pnt_idx * 3 + k];
+        }
+
+            // compute dft
+        if (idx < nq) {
+            for (unsigned j = 0; j < np; j++) {
+			    float q_r = 0;
+			    for (unsigned k = 0; k < 3; k++) 
+				    q_r += qvals[3 * idx + k] * shamem[3 * j + k];
+			    ft[idx] = ft[idx] + Cexpf(NEG_I * q_r); 
+		    }
+	    }
+    }
 }
 
-void cudft(unsigned npts, float * pts, unsigned nq, float * qvals,
-			complex_t * output) {
+py::array gpu_dft(np_array_t Pts, np_array_t qVals) {
+
+    if ((Pts.ndim() != 2) or qVals.ndim() != 2)
+        throw std::runtime_error("Input arrays must be 2-D numpy arrays");
+
+    if ((Pts.shape()[1] != 3) or (qVals.shape()[1] != 3))
+        throw std::runtime_error("input arrays must of shape [N, 3]");
+
+    /* size of input arrays */
+    unsigned npts = Pts.shape()[0];
+    unsigned nq = qVals.shape()[0];
+
+    /* NumPy  will allocate the buffer */
+    auto result = py::array_t<complex_t>(nq);
+
+    float * pts = (float *) Pts.request().ptr;
+    float * qvals = (float *) qVals.request().ptr;
+    complex_t * ft = (complex_t *) result.request().ptr;
+	
+    // copy beam-radius (squared) to constant memory
+    //float brsq = beam_radius * beam_radius;
+    //cudaMemcpyToSymbol(rsq, &brsq, sizeof(float), 0, cudaMemcpyHostToDevice);
 
 	// allocate memory on device
 	float * dpts, * dqvals;
@@ -36,18 +75,20 @@ void cudft(unsigned npts, float * pts, unsigned nq, float * qvals,
 	// allocate memory for output
 	cucomplex_t * dft = NULL;
 	cudaMalloc((void **) &dft, sizeof(cucomplex_t) * nq);
+    cudaMemset(dft, sizeof(cucomplex_t) * nq, 0);
 
 	// device parameters
-	unsigned threads = 256;
+	unsigned threads = N_PTS;
 	unsigned blocks = nq / threads; 
 	if (nq % threads != 0) blocks++;
 	gpuDFT<<< blocks, threads >>> (npts, dpts, nq, dqvals, dft);
 
 	// copy results back to host
-	cudaMemcpy(output, dft, sizeof(complex_t) * nq, cudaMemcpyDeviceToHost);
+	cudaMemcpy(ft, dft, sizeof(complex_t) * nq, cudaMemcpyDeviceToHost);
 
 	// free memory
 	cudaFree(dpts);
 	cudaFree(dqvals);
 	cudaFree(dft);
+    return result;
 }
