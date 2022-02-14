ca7850afbbc9fb8b4a897017d0d2095ce17d3494
Dinesh Kumar <dkumar@cam.lbl.gov>
2022-02-14 13:39:03 -0800

    Fixed repeated use of i counter. Fixed saving q-points

--- xpcs/calc_gisaxs.py
+++ xpcs/calc_gisaxs.py
@@ -18,14 +18,13 @@ if __name__ == '__main__':
 
     det = Detector()
     energy = 10000
-    wavelen = 1.23984E+04/energy
     sdd = 2
     alphai = (math.pi / 180.) * 0.2
     center = [0, 256]
     outfile = 'gixpcs.h5'
     refidx = 4.88E-06 + 1j * 7.37E-08
 
-    # move md locations to different origin and scale
+    # shift and scale for point positions
     origin = [8, 8, 8]
     scale = 28
 
@@ -35,16 +34,18 @@ if __name__ == '__main__':
     txtfiles = list_lammps_txt_files(datadir, filename_pattern)
     Nsteps = len(txtfiles)
     print('Number of steps', Nsteps)
-    
 
     # output hdf5 file
     h5f = h5py.File(outfile, 'w')
     grp = h5f.create_group('gixpcs')
-    qtmp = grp.create_dataset('q_points', (3, *det.shape), 'f')
+    qtmp = grp.create_dataset('q_points', (*det.shape, 3), 'f')
     dset = grp.create_dataset('imgs', (Nsteps, *det.shape), 'f')    
 
     # undistorted q-vectors
+    wavelen = 1.23984E+04/energy
     qvecs = det.dwba_qvectors(sdd, center, wavelen, alphai)
+    qtmp[:] = qvecs[0].reshape(*det.shape, 3)
+    
 
     # dwba coefficients
     thata, alpha = det.angles(sdd, center)
@@ -52,17 +53,14 @@ if __name__ == '__main__':
     
     # turn the crank
     t0 = time.time()
-    Nsteps = 2
     for i in range(Nsteps):
         mdsim = load_lammps_txt(txtfiles[i], origin=origin, scale=scale)
         pts = mdsim['POSITIONS']
         ff = np.zeros_like(thata, dtype=np.complex_)
-        for i in range(4):
-            ff += fc[i] * mdscatter.dft(pts, qvecs[i])
+        for j in range(4):
+            ff += fc[j] * mdscatter.dft(pts, qvecs[j])
         img = np.abs(ff)**2
         img = np.reshape(img, det.shape)
-        plt.imshow(np.log(img+1), origin='lower')
-        plt.show()
         dset[i,:,:] = np.reshape(img, det.shape)
     t1 = time.time() - t0
     print('time taken = %f\n' % t1)
